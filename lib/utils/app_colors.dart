1bda86f69b5f03a7cd23f9d2bb3ce02d3214ac11
Adil Nauruzbaev <aresbitch@mail.ru>
2024-08-21 15:21:24 +0500

    Theme changes

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color lightBackgroundColor = Color(0xFFFFFFFF);
+  static const Color lightPrimaryColor = Color(0xFF4CAF50);
+  static const Color lightAccentColor = Color(0xFF2196F3);
+  static const Color lightTextColor = Color(0xFF333333);
+
+  static const Color darkBackgroundColor = Color(0xFF121212);
+  static const Color darkPrimaryColor = Color(0xFF4CAF50);
+  static const Color darkAccentColor = Color(0xFF2196F3);
+  static const Color darkTextColor = Color(0xFFFFFFFF);
+}
