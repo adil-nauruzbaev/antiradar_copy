ddd11c4e131bb51e20fdfad19e120993c31bf93e
Adil Nauruzbaev <aresbitch@mail.ru>
2024-08-23 04:57:26 +0500

    small fix

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:isar/isar.dart';
-
-late Isar isar;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
