4e7180378ab2de92487fd1e20c561df3ce0610dc
Adil Nauruzbaev <aresbitch@mail.ru>
2024-08-26 22:07:22 +0500

    Added country saver

--- lib/presentation/view/on_boarding/country_select_screen.dart
+++ lib/presentation/view/on_boarding/country_select_screen.dart
@@ -1,14 +1,55 @@
 import 'dart:math';
 
+import 'package:antiradar/data/firebase/argentina_provider.dart';
+import 'package:antiradar/data/source/database/country_pod.dart';
+import 'package:antiradar/data/source/database/isar_service.dart';
+import 'package:antiradar/presentation/router/app_router.dart';
 import 'package:antiradar/presentation/view/on_boarding/widgets/continue_button.dart';
+import 'package:antiradar/presentation/view_model/isar/models/country_model.dart';
 import 'package:antiradar/presentation/view_model/settings/gradient_extension.dart';
 import 'package:antiradar/utils/app_fonts.dart';
+import 'package:antiradar/utils/extensions/localization.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
+import 'package:isar/isar.dart';
+
+enum CountryEnum {
+  argentina('assets/icons/country_flags/Argentina.svg'),
+  brazil('assets/icons/country_flags/Brazil.svg'),
+  uruguay(
+    'assets/icons/country_flags/Uruguay.svg',
+  ),
+  paraguay(
+    'assets/icons/country_flags/Paraguay.svg',
+  ),
+  chile('assets/icons/country_flags/Chile.svg'),
+  ;
+
+  final String flagPath;
+
+  const CountryEnum(this.flagPath);
+
+  String locale() {
+    final locale = navigatorKey.currentState!.context.localization;
+    return switch (this) {
+      argentina => locale.argentina,
+      brazil => locale.brazil,
+      uruguay => locale.uruguay,
+      paraguay => locale.paraguay,
+      chile => locale.chile,
+    };
+  }
+}
+
+enum DownloadEnum {
+  isDefault,
+  downloaded,
+  downloading,
+}
 
 class CountrySelectScreen extends ConsumerWidget {
   const CountrySelectScreen({super.key});
@@ -36,31 +77,38 @@ class CountrySelectScreen extends ConsumerWidget {
                 const SizedBox(
                   height: 40,
                 ),
-                _buildLanguageItem(
-                  context,
-                  loc.argentina,
-                  'assets/icons/country_flags/Argentina.svg',
-                ),
-                _buildLanguageItem(
-                  context,
-                  loc.brazil,
-                  'assets/icons/country_flags/Brazil.svg',
-                ),
-                _buildLanguageItem(
-                  context,
-                  loc.uruguay,
-                  'assets/icons/country_flags/Uruguay.svg',
-                ),
-                _buildLanguageItem(
-                  context,
-                  loc.paraguay,
-                  'assets/icons/country_flags/Paraguay.svg',
-                ),
-                _buildLanguageItem(
-                  context,
-                  loc.chile,
-                  'assets/icons/country_flags/Chile.svg',
-                ),
+                ...CountryEnum.values.map(
+                  (e) => DownloadWidget(
+                    country: e.locale(),
+                    flagPath: e.flagPath,
+                    countryDownload: e.name,
+                  ),
+                )
+                // _buildLanguageItem(
+                //   context,
+                //   loc.argentina,
+                //   'assets/icons/country_flags/Argentina.svg',
+                // ),
+                // _buildLanguageItem(
+                //   context,
+                //   loc.brazil,
+                //   'assets/icons/country_flags/Brazil.svg',
+                // ),
+                // _buildLanguageItem(
+                //   context,
+                //   loc.uruguay,
+                //   'assets/icons/country_flags/Uruguay.svg',
+                // ),
+                // _buildLanguageItem(
+                //   context,
+                //   loc.paraguay,
+                //   'assets/icons/country_flags/Paraguay.svg',
+                // ),
+                // _buildLanguageItem(
+                //   context,
+                //   loc.chile,
+                //   'assets/icons/country_flags/Chile.svg',
+                // ),
               ],
             ),
           ),
@@ -68,26 +116,101 @@ class CountrySelectScreen extends ConsumerWidget {
       ),
     );
   }
+}
 
-  Widget _buildLanguageItem(
-      BuildContext context, String country, String flagPath) {
+class DownloadWidget extends ConsumerStatefulWidget {
+  const DownloadWidget({
+    super.key,
+    required this.countryDownload,
+    required this.country,
+    required this.flagPath,
+  });
+  final String countryDownload;
+  final String country;
+  final String flagPath;
+  @override
+  ConsumerState<DownloadWidget> createState() => _DownloadState();
+}
+
+class _DownloadState extends ConsumerState<DownloadWidget> {
+  String get countryDownload => widget.countryDownload;
+
+  late final StateProvider<DownloadEnum> downloadPod;
+
+  @override
+  void initState() {
+    super.initState();
+    downloadPod = StateProvider((ref) {
+      final Isar isar = IsarDatabaseService().isarDB;
+      final length = isar.countryModels
+          .where()
+          .filter()
+          .countryEqualTo(countryDownload)
+          .countSync();
+      if (length > 0) {
+        return DownloadEnum.downloaded;
+      }
+      return DownloadEnum.isDefault;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final isDownloaded = ref.watch(downloadPod);
     return ListTile(
       contentPadding: const EdgeInsets.only(left: 16, right: 48, bottom: 24),
       title: Text(
-        country,
+        widget.country,
         style: AppFonts.langStyle,
       ),
       leading: SvgPicture.asset(
-        flagPath,
+        widget.flagPath,
         width: 40,
         height: 40,
       ),
-      trailing: Random().nextBool()
-          ? const _LoadIcon(
-              path: 'assets/icons/loaded.svg',
-            )
-          : const _LoadIcon(path: 'assets/icons/load.svg'),
-      onTap: () {},
+      trailing: switch (isDownloaded) {
+        DownloadEnum.isDefault =>
+          const _LoadIcon(path: 'assets/icons/load.svg'),
+        DownloadEnum.downloaded => const Icon(
+            Icons.check,
+            color: Colors.white,
+          ),
+        DownloadEnum.downloading => const _LoadIcon(
+            path: 'assets/icons/loaded.svg',
+          ),
+      },
+      onTap: isDownloaded == DownloadEnum.downloaded &&
+              isDownloaded == DownloadEnum.downloading
+          ? null
+          : () async {
+              try {
+                ref
+                    .read(downloadPod.notifier)
+                    .update((state) => DownloadEnum.downloading);
+                final country = widget.countryDownload;
+
+                if (country != null) {
+                  final models =
+                      await ref.read(firebaseModelsProvider(country).future);
+                  await ref
+                      .read(countryNotifierProvider(country).notifier)
+                      .saveAll(models)
+                      .whenComplete(() {
+                    ref
+                        .read(downloadPod.notifier)
+                        .update((state) => DownloadEnum.downloaded);
+                  });
+                } else {}
+
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('Data saved to Isar')),
+                );
+              } catch (e) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Error: $e')),
+                );
+              }
+            },
     );
   }
 }
@@ -116,19 +239,19 @@ class TopCountryBar extends ConsumerWidget {
     final loc = AppLocalizations.of(context)!;
 
     return Container(
-      padding: const EdgeInsets.only(left: 6, right: 24),
+      padding: const EdgeInsets.only(left: 6, right: 6),
       height: 60,
-      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-        IconButton(
-          onPressed: () {
-            context.pop();
-          },
-          icon: SvgPicture.asset(
-            'assets/icons/arrow.svg',
-            width: 12,
-            height: 20,
-          ),
-        ),
+      child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+        // IconButton(
+        //   onPressed: () {
+        //     context.pop();
+        //   },
+        //   icon: SvgPicture.asset(
+        //     'assets/icons/arrow.svg',
+        //     width: 12,
+        //     height: 20,
+        //   ),
+        // ),
         Text(
           loc.selectCountry,
           style: AppFonts.h3Style,
